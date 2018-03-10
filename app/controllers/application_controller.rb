8132b34d3eb45722d91c8589f3cfe643e2f823ee
Lina Hamed <lina.hamedahmad@gmail.com>
2018-03-10 10:32:49 +0200

    Login,Signup and Edit Pages

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :sanitize_devise_params, if: :devise_controller?
+
+  def sanitize_devise_params
+  	devise_parameter_sanitizer.permit(:sign_up , keys: [:name, :image])
+  	devise_parameter_sanitizer.permit(:account_update , keys: [:name, :image])
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100#" }
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+   
+end
