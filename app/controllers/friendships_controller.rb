d0974791c85b1d973b17396205c604c4fc4f4a2d
mohamed3laa33 <mohamedabdelshakor1@gmail.com>
2018-03-11 17:37:07 +0200

    Freindships in freinships views

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -17,6 +17,6 @@ class FriendshipsController < ApplicationController
     @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
     flash[:notice] = "Removed friendship."
-    redirect_to current_user
+    redirect_to root_url
   end
 end
