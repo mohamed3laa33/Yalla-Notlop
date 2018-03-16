733376c5f907e89a67a2693ec1e716b66f42a53b
rokaiaMohamed <engrokaia.mohamed@gmail.com>
2018-03-16 04:07:36 +0200

    fix error show group view

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180314165609) do
-
+ActiveRecord::Schema.define(version: 20180315204234) do
 
   create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.integer "user_id"
-
   end
 
   create_table "friends", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
